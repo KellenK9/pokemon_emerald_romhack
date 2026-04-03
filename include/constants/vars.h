6383435983671e15561bda8d2a04859aaad61d6d
KellenK9 <55595944+KellenK9@users.noreply.github.com>
2026-04-03 13:52:43 -0500

    Added missions in Birch's lab

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -104,7 +104,7 @@
 #define DN_VAR_STEP_COUNTER                              0x4052 // DexNav: Steps for finding hidden pokemon
 #define VAR_LAVARIDGE_TOWN_STATE                         0x4053
 #define VAR_CURRENT_SECRET_BASE                          0x4054 // was probably allocated for VAR_FALLARBOR_TOWN_STATE at one point
-#define VAR_VERDANTURF_TOWN_STATE                        0x4055 // Unused Var
+#define VAR_LITTLEROOT_MISSIONS                          0x4055 // Added for Romhack
 #define VAR_PACIFIDLOG_TOWN_STATE                        0x4056 // Unused Var
 #define VAR_PETALBURG_CITY_STATE                         0x4057
 #define VAR_SLATEPORT_CITY_STATE                         0x4058
