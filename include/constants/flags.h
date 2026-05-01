ddc033718aec89c1da06ca5d1c23257c84cd60b5
KellenK9 <55595944+KellenK9@users.noreply.github.com>
2026-05-01 15:08:39 -0500

    Birch gives stuff

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -263,7 +263,7 @@
 #define FLAG_RECEIVED_TM_ROAR                0xE7
 #define FLAG_RECEIVED_TM_GIGA_DRAIN          0xE8
 
-#define FLAG_UNUSED_0x0E9                    0xE9 // Unused Flag
+#define FLAG_BIRCH_INTRO_COMPLETE            0xE9 // Added for Romhack
 
 #define FLAG_RECEIVED_TM_REST                0xEA
 #define FLAG_RECEIVED_TM_ATTRACT             0xEB
